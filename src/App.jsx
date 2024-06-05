60d4235bade83cedbc31f566aac36c57e9d0fd61
PiPo <pipo.khomgrid@gmail.com>
2024-06-05 12:52:16 +0700

    add input handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [message, setMessage] = useState("Greeting Message");
+
+  function handleInputMessage(event) {
+    setInputMessage(event.target.value);
+  }
+  function handleMessage() {
+    setMessage(inputMessage);
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          value={inputMessage}
+          onChange={handleInputMessage}
+          id="greeting-message"
+          type="text"
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleMessage}>Update text</button>
       </div>
     </div>
   );
